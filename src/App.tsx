5ccbc08bda2d9972b34eb9f5383741a1464df6e9
JagTheFriend <jagthefriend12@gmail.com>
2021-11-20 11:58:52 +0200

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { v4 as uuid4 } from "uuid";
 import TextEditor from "./Components/TextEditor";
 import "./style.css";
 
 function App() {
-    return <TextEditor />; // <div className="App">Hello world</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/document/:id" element={<TextEditor />} />
+                <Route path="/" element={<Navigate to={`/document/${uuid4()}`} />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
