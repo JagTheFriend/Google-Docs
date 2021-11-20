e855fa944c9dfa30317410ce6c85557cd8214cc0
JagTheFriend <jagthefriend12@gmail.com>
2021-11-20 11:26:02 +0200

    The changes are now being sent to the sever

--- src/Components/TextEditor.tsx
+++ src/Components/TextEditor.tsx
@@ -1,7 +1,8 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Quill from "quill";
-import "quill/dist/quill.snow.css";
+import Delta from "quill-delta";
 import { io } from "socket.io-client";
+import "quill/dist/quill.snow.css";
 
 const TOOLBAR_OPTIONS = [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -16,23 +17,44 @@ const TOOLBAR_OPTIONS = [
 ];
 
 export default function TextEditor() {
-    useEffect(() => {
-        const socket = io("http://localhost:3001");
-        return () => {
-            socket.disconnect();
-        };
-    }, []);
-    // Run this function only once
-    const wrapperRef = useCallback((wrapper: HTMLDivElement) => {
-        if (wrapper === null) return;
+    const [socket, setSocket] = useState(null as unknown as typeof io);
+    const [quill, setQuill] = useState(null as unknown as Quill);
 
+    const wrapperRef = useCallback((wrapper) => {
+        if (wrapper == null) return;
         wrapper.innerHTML = "";
         const editor = document.createElement("div");
         wrapper.append(editor);
-        new Quill(editor, {
-            theme: "snow",
-            modules: { toolbar: TOOLBAR_OPTIONS },
-        });
+
+        // setup the quill editor window
+        setQuill(
+            new Quill(editor, {
+                theme: "snow",
+                modules: { toolbar: TOOLBAR_OPTIONS },
+            })
+        );
     }, []);
+
+    useEffect(() => {
+        // connect to the server
+        const s = io("http://localhost:3001");
+        setSocket(s);
+        return () => {
+            s.disconnect();
+        };
+    }, []);
+
+    useEffect(() => {
+        // make sure that socket and quill is defined
+        if (socket == null || quill == null) return;
+        const handler = (delta: Delta, oldDelta: Delta, source: String) => {
+            if (source !== "user") return;
+            socket.emit("send-changes", delta);
+        };
+        quill.on("text-change", handler);
+        return () => {
+            quill.off("text-change", handler);
+        };
+    }, [socket, quill]);
     return <div className="container" ref={wrapperRef}></div>;
 }
