51d4a0c5885d93c94fff1359837ef3992776d513
JagTheFriend <jagthefriend12@gmail.com>
2021-11-20 13:03:00 +0200

    Working on way to connect to MongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,10 @@
 import { Server, Socket } from "socket.io";
+import { connect } from "mongoose";
+import Document from "./Document";
+
+// connect to database
+connect("mongodb://localhost:27017/google-docs");
+const defaultValue = "";
 
 const io: Server = require("socket.io")(3001, {
     cors: {
@@ -6,15 +12,28 @@ const io: Server = require("socket.io")(3001, {
         method: ["GET", "POST"],
     },
 });
-
 io.on("connection", (socket: Socket) => {
-    socket.on("get-document", (documentId: string) => {
-        const data = "";
+    socket.on("get-document", async (documentId: string) => {
+        const document = await findOrCreateDocument(documentId);
+
         socket.join(documentId);
-        socket.emit("load-document", data);
+        socket.emit("load-document", document.data);
+
         socket.on("send-changes", (delta) => {
             // send the changes to a specific room/document
             socket.broadcast.to(documentId).emit("receive-changes", delta);
         });
+
+        socket.on("save-document", async (data: string) => {
+            await Document.findByIdAndUpdate(documentId, { data });
+        });
     });
 });
+
+async function findOrCreateDocument(id: string) {
+    if (id === null) return;
+    const document = await Document.findById(id);
+    if (document) return document;
+    // create document
+    return await document.create({ _id: id, data: defaultValue });
+}
