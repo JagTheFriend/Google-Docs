0b7bd02772540739e9b8adb3412ea7298e4b208b
JagTheFriend <jagthefriend12@gmail.com>
2021-11-20 11:26:31 +0200

    The changes can now be received from the client

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
-import { Socket } from "socket.io";
+import { Server, Socket } from "socket.io";
 
-const io: Socket = require("socket.io")(3001, {
+const io: Server = require("socket.io")(3001, {
     cors: {
         origin: "http://localhost:3000",
         method: ["GET", "POST"],
     },
 });
 
-io.on("connection", (socket) => {
+io.on("connection", (socket: Socket) => {
     console.log("Connected!");
+    socket.on("send-changes", (delta) => {
+        console.log(delta);
+    });
 });
